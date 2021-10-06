4e88a01dd0f5ffcdc4a01bb13ac6568e8e9172df
Zhifeng JIANG <samuel@Paprikas-MacBook-Pro.local>
2021-10-06 10:53:42 +0800

    My script.

--- src/jvm_maven_linux_zhifeng.sh
+++ src/jvm_maven_linux_zhifeng.sh
@@ -12,3 +12,17 @@ sudo update-alternatives --install /usr/bin/javac javac ${JAVA_HOME%*/}/bin/java
 wget https://dlcdn.apache.org/maven/maven-3/3.8.3/binaries/apache-maven-3.8.3-bin.tar.gz -P $HOME
 sudo tar xf $HOME/apache-maven-*.tar.gz -C $HOME
 sudo ln -s $HOME/apache-maven-3.6.3 $HOME/maven
+
+sudo mkdir -p /etc/profile.d
+sudo bash -c 'cat > /etc/profile.d/maven.sh << EOF
+JAVA_HOME=$HOME/jvm/jdk1.8.0_192
+export M2_HOME=$HOME/maven
+MAVEN_HOME=$HOME/maven
+export PATH=${M2_HOME}/bin:${PATH}
+EOF
+'
+
+sudo chmod +x /etc/profile.d/maven.sh
+source /etc/profile.d/maven.sh
+mvn --version
+
